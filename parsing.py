a1a845ef45cfcca90bb1078fe99f822c29e871e5
Gadik <Gadik@GADIK>
2023-10-30 22:09:37 +0400

    rename files

--- parsing.py
+++ parsing.py
@@ -1,15 +1,13 @@
-from bs4 import BeautifulSoup
+import os
+import time
+
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import requests
-import time
-import os
-
 
 
 def get_data(request):
     url = f"https://yandex.ru/images/search?text={request}"
-    os.mkdir(f"dataset/{request}")
     headers = {
         "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Encoding":"gzip, deflate, br",
@@ -19,47 +17,64 @@ def get_data(request):
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0"
     }
     driver = webdriver.Firefox()
-    driver.get(url=url)
+    driver.get(url = url)
     driver.maximize_window()
     time.sleep(3)
+    driver.find_element(By.CSS_SELECTOR, 'div.serp-item__preview a.serp-item__link').click()
 
-    with open(f"urls_{request}.txt", 'w') as file:  #скачивание ссылок
-        for i in range(0,1500):
+    with open(f"urls_{request}.txt", 'w') as file:
+        for i in range(0, 1500):
             time.sleep(0.05)
-            #driver.find_element(By.CSS_SELECTOR, "a.Button2_view_action").click() #открыть фотку
-
-            link = driver.find_element(By.CSS_SELECTOR,"a.Button2_view_action").get_attribute("href")
+            link = driver.find_element(By.CSS_SELECTOR, "a.Button2_view_action").get_attribute("href")
             print(i)
             file.write(link + '\n')
-            #iter+=1
-            driver.find_element(By.CSS_SELECTOR, "div.CircleButton:nth-child(4)").click() #следующая фотка
- 
+            driver.find_element(By.CSS_SELECTOR, "div.CircleButton:nth-child(4)").click()
+
     driver.close()
     driver.quit()
 
+
 def get_links(request):
-    iter = 586
-    i = 1
+    count = 0
+    os.mkdir(f"dataset/{request}")
+
     with open(f"urls_{request}.txt", "r") as file:
         for line in file:
-            url = line.strip()
-            if iter<i:
+            try:
+                url = line.strip()
                 time.sleep(1)
-                response = requests.get(url, verify=False)
+                response = requests.get(url)
                 if response.status_code == 200:
-                    with open(f"{request}/{iter}.jpg", "wb") as image_file:
+                    with open(f"dataset/{request}/{count}.jpg", "wb") as image_file:
                         image_file.write(response.content)
-                    print(f'{iter} успешно скачано.')
-                    iter+=1
+                    print(f'{count} успешно скачано')
+                    count+=1
                 else:
                     print(f'Ошибка при скачивании изображения с URL: {url}')
-            print(i)
-            i+=1
+            except:
+                continue
+
+
+def rename_files(request):
+    folder_path = f"dataset/{request}"
+    count = 0
+
+    for filename in os.listdir(folder_path):
+        old_file_path = os.path.join(folder_path, filename)
+        number = str(count)
+        new_filename = number.zfill(4) + ".jpeg"
+        new_file_path = os.path.join(folder_path, new_filename)
+        os.rename(old_file_path, new_file_path)
+        print (f"{count} переименовано")
+        count += 1
 
 
 def main():
-    #get_data("leopard")
-    get_links("tiger")
+    request = input("Введите запрос: ")
+    get_data(request)
+    get_links(request)
+    rename_files(request)
+
 
 if __name__ == "__main__":
     main()
